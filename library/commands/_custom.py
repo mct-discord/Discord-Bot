4f142ab1d5c700cdd0e7c9e951d8b1199bcc50aa
nielsdewulf <niels.dewulf2@student.howest.be>
2019-11-02 19:10:02 +0100

    Added send type to custom command

--- library/commands/_custom.py
+++ library/commands/_custom.py
@@ -5,16 +5,21 @@ from library.utilities.userhelper import UserHelper
 
 
 class Custom(Command):
-    
+
     def __init__(self, bot, name, type, action, value):
-        super().__init__(name,bot)
+        super().__init__(name, bot)
         self.type = type
         self.action = action
         self.action_value = value
         self.bot = bot
         self.allowed_sources = [DMChannel, TextChannel]
-        
-    async def on_execute(self, ctx, params):     
+
+    async def on_execute(self, ctx, params):
+        if self.type == "send":
+            if self.action == "message":
+                await ctx.channel.send(self.action_value)
+            if self.action == "dm":
+                await ctx.channel.send(self.action_value)
         if self.type == "role":
             if self.action == "add":
                 await UserHelper(self.bot).add_role(ctx.author, uid=int(self.action_value))
