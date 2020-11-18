83588131a5190ad3445b8cdf64443fac79038462
Niels Dewulf <niels.dewulf2@student.howest.be>
2020-11-18 20:10:48 +0100

    Fixed Google Spreadsheets bug

--- bot/library/utilities/gspreadsheets.py
+++ bot/library/utilities/gspreadsheets.py
@@ -1,112 +1,102 @@
 import json
-import gspread
 import datetime
 from oauth2client.service_account import ServiceAccountCredentials
-
+from googleapiclient.discovery import build
 
 class GSpreadsheets:
 
-    def __init__(self, keypath):
+    def __init__(self, keypath, spreadsheetId):
         self.keypath = keypath
+        self.spreadsheetId = spreadsheetId
 
     def authorize(self):
-        scope = ['https://spreadsheets.google.com/feeds']
+        scope = ['https://www.googleapis.com/auth/spreadsheets']
 
         credentials = ServiceAccountCredentials.from_json_keyfile_name(
             self.keypath, scope)
 
-        self.client = gspread.authorize(
-            credentials)  # authenticate with Google
+
+        self.service = build('sheets', 'v4', credentials=credentials)
 
     def add_warning(self, to_warn, reason, given_by):
         self.authorize()
-
-        def next_available_row(worksheet):
-            str_list = list(filter(None, worksheet.col_values(1)))
-            return str(len(str_list)+1)
-        # gspread.Client.open_by_url
-        # json credentials you downloaded earlier
-
-        worksheet = self.client.open_by_url(
-            url="https://docs.google.com/spreadsheets/d/1pB7nv1Oaf8En7YOLiqiKny9tOTli42Psvpn8-RibBXU/edit?usp=sharing").get_worksheet(0)
-        next_row = next_available_row(worksheet)
-
-        # insert on the next available row
-        worksheet.update_acell("A{}".format(next_row),
-                               str(datetime.datetime.now()))
-        worksheet.update_acell("B{}".format(next_row), str(to_warn.id))
-        worksheet.update_acell("C{}".format(next_row), to_warn.name)
-        worksheet.update_acell("D{}".format(next_row), reason)
-        worksheet.update_acell("E{}".format(next_row), given_by.name)
-        worksheet.update_acell("F{}".format(next_row), str(given_by.id))
+        
+        list = [[str(datetime.datetime.now()), str(to_warn.id), to_warn.name,reason,given_by.name,str(given_by.id)]]
+        resource = {
+            "majorDimension": "ROWS",
+            "values": list
+        }
+        range = "Warnings!A:F"
+        self.service.spreadsheets().values().append(
+        spreadsheetId=self.spreadsheetId,
+        range=range,
+        body=resource,
+        valueInputOption="USER_ENTERED"
+        ).execute()
 
     def add_kick(self, to_kick, reason, given_by):
         self.authorize()
-
-        def next_available_row(worksheet):
-            str_list = list(filter(None, worksheet.col_values(1)))
-            return str(len(str_list)+1)
-        # gspread.Client.open_by_url
-        # json credentials you downloaded earlier
-
-        worksheet = self.client.open_by_url(
-            url="https://docs.google.com/spreadsheets/d/1pB7nv1Oaf8En7YOLiqiKny9tOTli42Psvpn8-RibBXU/edit?usp=sharing").get_worksheet(2)
-        next_row = next_available_row(worksheet)
-
-        # insert on the next available row
-        worksheet.update_acell("A{}".format(next_row),
-                               str(datetime.datetime.now()))
-        worksheet.update_acell("B{}".format(next_row), str(to_kick.id))
-        worksheet.update_acell("C{}".format(next_row), to_kick.name)
-        worksheet.update_acell("D{}".format(next_row), reason)
-        worksheet.update_acell("E{}".format(next_row), given_by.name)
-        worksheet.update_acell("F{}".format(next_row), str(given_by.id))
+        
+        list = [[str(datetime.datetime.now()), str(to_kick.id), to_kick.name,reason,given_by.name,str(given_by.id)]]
+        resource = {
+            "majorDimension": "ROWS",
+            "values": list
+        }
+        range = "Kicks!A:F"
+        self.service.spreadsheets().values().append(
+        spreadsheetId=self.spreadsheetId,
+        range=range,
+        body=resource,
+        valueInputOption="USER_ENTERED"
+        ).execute()
 
     def add_ban(self, to_ban, reason, given_by):
         self.authorize()
-
-        def next_available_row(worksheet):
-            str_list = list(filter(None, worksheet.col_values(1)))
-            return str(len(str_list)+1)
-        # gspread.Client.open_by_url
-        # json credentials you downloaded earlier
-
-        worksheet = self.client.open_by_url(
-            url="https://docs.google.com/spreadsheets/d/1pB7nv1Oaf8En7YOLiqiKny9tOTli42Psvpn8-RibBXU/edit?usp=sharing").get_worksheet(3)
-        next_row = next_available_row(worksheet)
-
-        # insert on the next available row
-        worksheet.update_acell("A{}".format(next_row),
-                               str(datetime.datetime.now()))
-        worksheet.update_acell("B{}".format(next_row), str(to_ban.id))
-        worksheet.update_acell("C{}".format(next_row), to_ban.name)
-        worksheet.update_acell("D{}".format(next_row), reason)
-        worksheet.update_acell("E{}".format(next_row), given_by.name)
-        worksheet.update_acell("F{}".format(next_row), str(given_by.id))
+        
+        list = [[str(datetime.datetime.now()), str(to_ban.id), to_ban.name,reason,given_by.name,str(given_by.id)]]
+        resource = {
+            "majorDimension": "ROWS",
+            "values": list
+        }
+        range = "Bans!A:F"
+        self.service.spreadsheets().values().append(
+        spreadsheetId=self.spreadsheetId,
+        range=range,
+        body=resource,
+        valueInputOption="USER_ENTERED"
+        ).execute()
 
     def add_mute(self, to_mute, reason, given_by, length=None):
         self.authorize()
+        
+        if length is None:
+            length = 'Infinite'
+        
+        list = [[str(datetime.datetime.now()), str(to_mute.id), to_mute.name,reason,length,given_by.name,str(given_by.id)]]
+        resource = {
+            "majorDimension": "ROWS",
+            "values": list
+        }
+        range = "Mutes!A:G"
+        self.service.spreadsheets().values().append(
+        spreadsheetId=self.spreadsheetId,
+        range=range,
+        body=resource,
+        valueInputOption="USER_ENTERED"
+        ).execute()
 
-        def next_available_row(worksheet):
-            str_list = list(filter(None, worksheet.col_values(1)))
-            return str(len(str_list)+1)
-        # gspread.Client.open_by_url
-        # json credentials you downloaded earlier
-
-        worksheet = self.client.open_by_url(
-            url="https://docs.google.com/spreadsheets/d/1pB7nv1Oaf8En7YOLiqiKny9tOTli42Psvpn8-RibBXU/edit?usp=sharing").get_worksheet(1)
-        next_row = next_available_row(worksheet)
-
-        # insert on the next available row
-        worksheet.update_acell("A{}".format(next_row),
-                               str(datetime.datetime.now()))
-        worksheet.update_acell("B{}".format(next_row), str(to_mute.id))
-        worksheet.update_acell("C{}".format(next_row), to_mute.name)
-        worksheet.update_acell("D{}".format(next_row), reason)
-        if length is not None:
-            worksheet.update_acell("E{}".format(next_row), length)
-        else:
-            worksheet.update_acell("E{}".format(next_row), "Infinite")
-
-        worksheet.update_acell("F{}".format(next_row), given_by.name)
-        worksheet.update_acell("G{}".format(next_row), str(given_by.id))
+    def add_message(self, to_message, reason, given_by):
+        self.authorize()
+        
+        list = [[str(datetime.datetime.now()), str(to_message.id), to_message.name,reason,given_by.name,str(given_by.id)]]
+        resource = {
+            "majorDimension": "ROWS",
+            "values": list
+        }
+        range = "MessageLog!A:F"
+        self.service.spreadsheets().values().append(
+        spreadsheetId=self.spreadsheetId,
+        range=range,
+        body=resource,
+        valueInputOption="USER_ENTERED"
+        ).execute()
